e9894bb57ae19df21f1975c4bf3176fabe9e0704
Dayhana Ramirez <dayhanaramirez45@gmail.com>
2022-02-21 13:22:20 -0500

    Employee, SalaryEmployee, CommissionEmployee. (Class 2)

--- POOConcepts/POOConcepts/Program.cs
+++ POOConcepts/POOConcepts/Program.cs
@@ -3,13 +3,40 @@
 Console.WriteLine("POO Concepts");
 Console.WriteLine("============");
 
-try
+//try
+//{
+//    console.writeline(new date(2022, 12, 7));
+//    console.writeline(new date(2022, 9, 23));
+//    console.writeline(new date(2024, 2, 29));
+//}
+//catch (exception error)
+//{
+//    console.writeline(error.message);
+//}
+
+Employee employee1 = new SalaryEmployee()
 {
-    Console.WriteLine(new Date(2022, 12, 7));
-    Console.WriteLine(new Date(2022, 9, 23));
-    Console.WriteLine(new Date(2024, 2, 29));
-}
-catch (Exception error)
+    Id = 1010,
+    FirstName = "Dayhana",
+    LastName = "Ramirez",
+    Birthdate = new Date(1996, 5, 23),
+    HiringDate = new Date(2022, 1, 15),
+    IsActive = true,
+    Salary = 1248569.45M
+};
+
+Employee employee2 = new CommissionEmployee()
 {
-    Console.WriteLine(error.Message);
-}
+    Id = 1020,
+    FirstName = "Daniel",
+    LastName = "Ramirez",
+    Birthdate = new Date(1997, 12, 3),
+    HiringDate = new Date(2022, 2, 15),
+    IsActive = true,
+    Sales = 15635000M,
+    CommissionPercentaje = 0.2F
+};
+
+Console.WriteLine(employee1);
+Console.WriteLine();
+Console.WriteLine(employee2);
