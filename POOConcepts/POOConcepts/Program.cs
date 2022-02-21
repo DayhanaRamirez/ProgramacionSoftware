5d7006bfe5eade420ec7b8928bf1d14e654089e2
Dayhana Ramirez <dayhanaramirez45@gmail.com>
2022-02-20 20:47:07 -0500

    Finish date class. (Class 1)

---
+++ POOConcepts/POOConcepts/Program.cs
@@ -0,0 +1,15 @@
+﻿using POOConcepts;
+
+Console.WriteLine("POO Concepts");
+Console.WriteLine("============");
+
+try
+{
+    Console.WriteLine(new Date(2022, 12, 7));
+    Console.WriteLine(new Date(2022, 9, 23));
+    Console.WriteLine(new Date(2024, 2, 29));
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+}
